baa70ceec448b6e76cc2824f10b9919cfd4013d8
TarleyRodrigues <tarley.divino@gmail.com>
2022-04-26 19:42:24 -0300

    Payment, one-to-one association

--- src/main/java/com/estudandoferramentas/course/config/TestConfig.java
+++ src/main/java/com/estudandoferramentas/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.estudandoferramentas.course.entities.Category;
 import com.estudandoferramentas.course.entities.Order;
 import com.estudandoferramentas.course.entities.OrderItem;
+import com.estudandoferramentas.course.entities.Payment;
 import com.estudandoferramentas.course.entities.Product;
 import com.estudandoferramentas.course.entities.User;
 import com.estudandoferramentas.course.entities.enums.OrderStatus;
@@ -81,5 +82,9 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.saveAll(Arrays.asList(o1));
 	}
 }
