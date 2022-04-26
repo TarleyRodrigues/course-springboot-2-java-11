8c7c088d254e9b67f98d4dd7bbc84a55e33ab525
TarleyRodrigues <tarley.divino@gmail.com>
2022-04-26 17:21:04 -0300

    ProducMany-to-many association with JoinTable

--- src/main/java/com/estudandoferramentas/course/entities/Product.java
+++ src/main/java/com/estudandoferramentas/course/entities/Product.java
@@ -8,14 +8,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
-public class Product implements Serializable{
-private static final long serialVersionUID = 1L;
-	
+public class Product implements Serializable {
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -23,10 +25,11 @@ private static final long serialVersionUID = 1L;
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
-	@Transient
+
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
-	
+
 	public Product() {
 	}
 
@@ -106,6 +109,6 @@ private static final long serialVersionUID = 1L;
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}	
-	
+	}
+
 }
